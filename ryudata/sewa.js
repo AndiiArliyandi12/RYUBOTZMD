13066f2111ed8daa414a8467b683d1523e36ecec
kmtdlah <99109389+kmtdlah@users.noreply.github.com>
2022-03-27 21:58:57 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -1,23 +1,5 @@
 exports.sewa = () => {
-	return`🇸 🇪 🇼 🇦  🇧 🇴 🇹 ❗
+	return`KNTL :V ❗
 
-┏━━━━━━
-┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
-┃ *1 MINGGU : Rp 5.000*
-┃ *1 BULAN : Rp 15.000*
-┃ *PERMANEN : Rp 35.000*
-┗━━━━━━
-┏━━━━━━
-┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
-┃ *⚔️ Antilink Group*
-┃ *👨‍🎨 Buat Stiker*
-┃ *📥 Downloader*
-┃ *Dll.*
-┗━━━━━━
-
-*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
-
-*MINAT? CHAT :
-wa.me/6285609233482
 `
 }
